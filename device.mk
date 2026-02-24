76b0c98e7e7c2fcc97c54903749bfc4044dd3317
Alcatel 1T 7 9013x <u5847537051@gmail.com>
2026-02-24 16:37:07 +0200

    Device Tree Fix 7.0

--- device.mk
+++ device.mk
@@ -6,3 +6,12 @@
 #
 
 LOCAL_PATH := device/ihunt/iHuntS22ULTRA
+
+# Custom TWRP Theme
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/root/twres/ui.xml:recovery/root/twres/ui.xml
+
+# Copy all images and fonts
+# This is a shorthand; it's better to list them or use a wildcard if your build system supports it
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root/twres/images,recovery/root/twres/images)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root/twres/fonts,recovery/root/twres/fonts)
